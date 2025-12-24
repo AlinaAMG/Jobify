b38454de9771a9dccd29111d343ecb6affb4e879
Alina <alina_gabur@yahoo.com>
2025-12-24 13:54:55 +0100

    add mock data for jobs

---
+++ prisma/seed.js
@@ -0,0 +1,27 @@
+const { PrismaClient } = require('@prisma/client');
+const data = require('./mock-data.json');
+const prisma = new PrismaClient();
+
+const main = async () => {
+  const clerkId = 'user_371mkPTHa826rh8DiseKeHaRrar';
+  const jobs = data.map((job) => {
+    return {
+      ...job,
+      clerkId,
+    };
+  });
+  for (const job of jobs) {
+    await prisma.job.create({
+      data: job,
+    });
+  }
+};
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
